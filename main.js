bea81a535ebb05834279961b1db696b63f9f6677
Marcus Chong <marcus.jn.chong@gmail.com>
2018-09-22 18:36:18 -0400

    oh wowie it displays messages kind of asldfkjasdlfjslfkjdslkfdjsklfj

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log('sdflkdsjflksdjfklsdjflksdjflkj');
+var stream = new Vue({
+  el: '#stream',
+  data: {
+    items: [
+      { 
+        author: 'aaaaaa',
+        message: 'Foo'
+      },
+
+      { 
+        author: 'Bsdf',
+        message: 'Bar',
+      },
+
+      { 
+        author: 'dummy value',
+        message: 'also dummy value',
+      },
+
+      { 
+        author: 'really long really long dummy text really long dummy text really long dummy text ',
+        message: 'also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text ',
+      },
+    ]
+  }
+})
