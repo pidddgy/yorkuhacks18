e0b46ea8abea851b904ff892cacdfc19b0923627
Marcus Chong <marcus.jn.chong@gmail.com>
2018-09-22 18:40:46 -0400

    HELP WE LITERALLY WASTE LIEK 11 HOURS AND WE NEED TO GET STUFF DOBE LSK;DFJS;DLKJFASDL;FJSLDFJSDLFJSDLKFJSDLKFJSDLFKJSDLFKJSLFKJSDFLKSDJFLSKDJF

--- main.js
+++ main.js
@@ -3,22 +3,22 @@ var stream = new Vue({
   data: {
     items: [
       { 
-        author: 'aaaaaa',
+        title: 'aaaaaa',
         message: 'Foo'
       },
 
       { 
-        author: 'Bsdf',
+        title: 'Bsdf',
         message: 'Bar',
       },
 
       { 
-        author: 'dummy value',
+        title: 'dummy value',
         message: 'also dummy value',
       },
 
       { 
-        author: 'really long really long dummy text really long dummy text really long dummy text ',
+        title: 'really long really long dummy text really long dummy text really long dummy text ',
         message: 'also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text ',
       },
     ]
