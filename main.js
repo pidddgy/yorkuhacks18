0ae1796ad71a38603e4455e6df83f55bc67786ac
Marcus Chong <marcus.jn.chong@gmail.com>
2018-09-22 18:55:33 -0400

    AUTHOR IS DispalAYed iNh teXTgit add .git add .git add .! GUCCI GANG GUCCI GANG

--- main.js
+++ main.js
@@ -4,22 +4,26 @@ var stream = new Vue({
     items: [
       { 
         title: 'aaaaaa',
-        message: 'Foo'
+        message: 'Foo',
+        author: 'sf',
       },
 
       { 
         title: 'Bsdf',
         message: 'Bar',
+        author: 'Anonymous',
       },
 
       { 
         title: 'dummy value',
         message: 'also dummy value',
+        author: 'sdfdff',
       },
 
       { 
         title: 'really long really long dummy text really long dummy text really long dummy text ',
-        message: 'also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text ',
+        message: 'also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text ', 
+        author: 'sfsdfsdfsdff',
       },
     ]
   }
