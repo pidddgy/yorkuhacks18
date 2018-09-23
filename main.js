e6d75006bf7528550cfd2a9994425a91bd931afa
Marcus Chong <marcus.jn.chong@gmail.com>
2018-09-22 20:44:00 -0400

    wooooo it shows comments and it lets you show more/show less if there's more than 3

--- main.js
+++ main.js
@@ -6,24 +6,88 @@ var stream = new Vue({
         title: 'aaaaaa',
         message: 'Foo',
         author: 'sf',
+        showLess: true,
+        comments: [
+          {
+            author: "Bob",
+            text: "ur question dumb and you should feel bad",
+          },
+          {
+            author: "AAAAAAAA",
+            text: "bepis",
+          },
+          {
+            author: "AAAAAAAA",
+            text: "many amount of posts dummy text",
+          },
+          {
+            author: "AAAAAAAA",
+            text: "many amount of posts dummy text",
+          },
+          {
+            author: "AAAAAAAA",
+            text: "many amount of posts dummy text",
+          },
+          {
+            author: "AAAAAAAA",
+            text: "many amount of posts dummy text",
+          },
+          {
+            author: "AAAAAAAA",
+            text: "many amount of posts dummy text",
+          },
+        ],
       },
 
       { 
         title: 'Bsdf',
         message: 'Bar',
         author: 'Anonymous',
+        showLess: true,
+        comments: [
+          {
+            author: "Bob",
+            text: "ur question dumb and you should feel bad",
+          },
+          {
+            author: "AAAAAAAA",
+            text: "bepis",
+          },
+        ],
       },
 
       { 
         title: 'dummy value',
         message: 'also dummy value',
         author: 'sdfdff',
+        showLess: true,
+        comments: [
+          {
+            author: "joe",
+            text: "asdf hi",
+          },
+          {
+            author: "asdf;lkjasdflkajsdflksdf",
+            text: "very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment very long dummy text for comment ",
+          },
+        ],
       },
 
       { 
         title: 'really long really long dummy text really long dummy text really long dummy text ',
         message: 'also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text also really long dummy text ', 
         author: 'sfsdfsdfsdff',
+        showLess: true,
+        comments: [
+          {
+            author: "lil pump",
+            text: "gucci gang",
+          },
+          {
+            author: "AAAAAAAA",
+            text: "bepis",
+          },
+        ],
       },
     ]
   }
